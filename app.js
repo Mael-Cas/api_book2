828debb3f8299640829ff2f6a6e62f031beeab24
maelc <castellan.mael@efrei.net>
2024-09-30 18:41:40 -0400

    add morgan

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const UserRouter = require('./routes/User');
 const BookRouter = require('./routes/Book');
 const FavoritesRouter = require('./routes/Favorite');
 
 
 const app = express();
+app.use(morgan('combined'));
 app.use(express.json());
 
 app.use((req, res, next) => {
